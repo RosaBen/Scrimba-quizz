8496224821da90742bceeeb9ad96196ab5dddcbb
RosaBen <benchabane.rosa@outlook.com>
2026-08-20 10:47:49 +0200

    added css for selected options

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,8 +1,39 @@
-export default function Questions({ data, currentIndex, setIndex }) {
-  console.log(data);
+import clsx from "clsx";
+
+export default function Questions({
+  data,
+  // currentIndex,
+  // setIndex,
+  path,
+  answers,
+  setAnswers,
+}) {
   const questionsList = data.map((q, index) => {
+    const currentQuestion = index;
+    const correctAnswer = q.correct;
+    console.log(correctAnswer);
     const optionsList = q.options.map((opt, i) => {
-      return <button key={i}>{opt}</button>;
+      let classOption;
+      function handleSelect(value, questionIndex) {
+        const newAnswers = [...answers];
+        newAnswers[questionIndex] = value;
+        setAnswers(newAnswers);
+      }
+      if (answers.includes(opt) && path === "/quiz") {
+        classOption = clsx("option", "answer");
+      } else {
+        classOption = clsx("option");
+      }
+
+      return (
+        <button
+          key={i}
+          onClick={() => handleSelect(opt, currentQuestion)}
+          className={classOption}
+        >
+          {opt}
+        </button>
+      );
     });
     return (
       <div className={`question${index} question`} key={index}>
