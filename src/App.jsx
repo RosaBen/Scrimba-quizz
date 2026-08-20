5fd706b621ef24fbe348bef3f110a7ae5902f728
RosaBen <benchabane.rosa@outlook.com>
2026-08-20 15:47:35 +0200

    added api

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ import Results from "./pages/Results";
 
 // Import scripts
 import { quiz } from "./assets/scripts/data";
-import { shuffleArray } from "./assets/scripts/utils";
+import { shuffleArray, selectItems, decodeHtml } from "./assets/scripts/utils";
 
 // Styles
 import "./assets/styles/index.css";
 import "./assets/styles/pages.css";
+import "./assets/styles/responsive.css";
 
 function App() {
   const [data, setData] = useState([]);
@@ -21,11 +22,26 @@ function App() {
   const { pathname } = useLocation();
 
   useEffect(() => {
-    const newDataArray = quiz.map((obj) => ({
-      ...obj,
-      options: shuffleArray(obj.options),
-    }));
-    setData(newDataArray);
+    fetch(
+      "https://opentdb.com/api.php?amount=10&category=20&difficulty=easy&type=multiple",
+    )
+      .then((res) => res.json())
+      .then((d) => {
+        const results = d.results;
+        const selectedData = selectItems(results, 5);
+        const allData = selectedData.map((q) => {
+          const newArray = shuffleArray([
+            ...q.incorrect_answers,
+            q.correct_answer,
+          ]);
+          return {
+            question: decodeHtml(q.question),
+            correct: decodeHtml(q.correct_answer),
+            options: newArray,
+          };
+        });
+        setData(allData);
+      });
   }, []);
 
   return (
@@ -40,6 +56,7 @@ function App() {
               path={pathname}
               answers={answers}
               setAnswers={setAnswers}
+              decode={decodeHtml}
             />
           }
         />
--- src/assets/scripts/utils.js
+++ src/assets/scripts/utils.js
@@ -1,5 +1,20 @@
 // shuffle array
-
-export function shuffleArray (array) {
-  return array.sort(() => Math.random() - 0.5);
+export function shuffleArray (arr) {
+  if (!Array.isArray(arr)) { return []; }
+  return [...arr].sort(() => Math.random() - 0.5);
 }
+
+// select 5 questions from fetch results
+export function selectItems (arr, count) {
+  if (!Array.isArray(arr)) { return []; }
+  return shuffleArray(arr).slice(0, count);
+
+}
+
+// // decode entité html 
+// export function decodeHtml (text) {
+//   const textArea = document.createElement("textarea");
+//   textArea.innerHTML = text;
+//   return textArea.value;
+
+// }
